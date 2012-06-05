ec390ed4962adc377035fe30d734f93c52c75d67
Stan Mazhara <akmegran@gmail.com>
2012-06-05 07:40:49 -0700

    add pypi classifiers

--- setup.py
+++ setup.py
@@ -9,5 +9,14 @@ setup(
     license='LICENSE',
     description='Awesome print.',
     long_description=open('README.txt').read(),
-    url='https://github.com/smazhara/awesome_print'
+    url='https://github.com/smazhara/awesome_print',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+        ]
 )
